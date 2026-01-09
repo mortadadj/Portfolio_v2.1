4a97d41f7f74ce29d3bbc966e24bbf3545e55e86
Mortada Djebbouri <mortadadjebbourioran@gmail.com>
2026-01-09 22:09:38 +0100

    Fix project image paths for Vite production

--- src/components/3-main/myProjects.jsx
+++ src/components/3-main/myProjects.jsx
@@ -4,7 +4,7 @@ export const myProjects = [
   {
     projectTitle: "Agenc de Voyage ",
     category: ["css", "html","react", "js"],
-    imgPath: "../public/agance_V.png",
+    imgPath: "/agance_V.png",
     subTitel: "2 Un algorithme de détection des anomalies dans les images radiographiques des tumeurs cérébrales",
     resume: "c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ",
     paragraphe: "2 c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ,c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer s photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche 2 c'est t'un traitemt  la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui ai "
@@ -14,7 +14,7 @@ export const myProjects = [
   {
     projectTitle: " ia tumer",
     category: ["ia"],
-    imgPath: "../public/tumer.jpg",
+    imgPath: "/tumer.jpg",
     subTitel: "4 Un algorithme de détection des anomalies dans les images radiographiques des tumeurs cérébrales ",
     resume: "c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ",
     paragraphe: "   "
@@ -25,20 +25,13 @@ export const myProjects = [
   {
     projectTitle: "2 eux Educatifs",
     category: ["css","flutter"],
-    imgPath: "../public/flater_jeux.jpg",
+    imgPath: "/flater_jeux.jpg",
     subTitel: "4 Un algorithme de détection des anomalies dans les images radiographiques des tumeurs cérébrales ",
     resume: "c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ",
     paragraphe: "3 c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ,c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche   "
   },
 
-  {
-    projectTitle: "ia   flutter,",
-    category: ["ia","flutter"],
-    imgPath: "../public/tumer1.jpg",
-    subTitel: "ia   flutter,",
-    resume: "c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem qui aide dans la detection tumer ce algorimte base sur traitemt image qui divise le cerveu humainne verticale et qu'est compare la partie daroite du cerveua par raport gauche  ",
-    paragraphe: "1 c'est t'un traitemt des images photoradio qui contient un tuymer du cerveu , on a creer un algorithem "
-  },
+
 
   
 
